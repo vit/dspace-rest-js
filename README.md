aea1181b5b6207c1d30ab72a9c415d323358c10f
vit2 <shiegin@gmail.com>
2018-01-01 23:56:52 +0000

    initial

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # dspace-rest-js
+
+Simple DSpace REST API client for node, only reading supported
+
+## Installation
+
+```shell
+  npm install @billypilgrim/dspace-rest-js --save
+```
+
+## Usage example (with express and jade)
+
+### index.js (express router)
+
+```js
+var dspace = require('@billypilgrim/dspace-rest-js');
+var conn = dspace.connect("https://dspace.example.com", "/rest")
+
+router.get('/', function(req, res, next) {
+    conn.getItem().then( item => res.render('index', { item }) );
+});
+
+router.get('/:type/:id', function(req, res, next) {
+    conn.getItem(req.params.type, req.params.id).then( item => {
+        let title = item.name;
+        res.render('index', { item, title })
+    } );
+});
+```
+
+### index.jade (jade template)
+
+```js
+  if item.uuid
+    a(href="/") Root
+    | >
+    each ancestor in item.ancestors
+      - let path = "/" + ancestor.type + "/" + ancestor.uuid
+      a(href=path)= ancestor.name
+      | >
+
+  h1= item.name || "Electronic Library"
+  if item.communities && item.communities.length > 0
+    h2 Communities
+    ul
+    each val in item.communities
+      li
+        a(href="/communities/"+val.uuid)= val.name
+
+  if item.collections && item.collections.length > 0
+    h2 Collections
+    ul
+    each val in item.collections
+      li
+        a(href="/collections/"+val.uuid)= val.name
+
+  if item.items && item.items.length > 0
+    h2 Items
+    ul
+    each val in item.items
+      li
+        a(href="/items/"+val.uuid)= val.name
+
+  if item.metadata && item.metadata.length > 0
+    h2 Metadata
+    table
+      thead
+        th Key
+        th Value
+        th Language
+      tbody
+        each val in item.metadata
+          tr
+            td= val.key
+            td= val.value
+            td= val.language
+
+  if item.bitstreams && item.bitstreams.length > 0
+    h2 Bitstreams
+    ul
+    each val in item.bitstreams
+      li
+        =val.bundleName + ": "
+        a(href=val.retrieveUrl)= val.name
+        = " " + val.sizeBytes
+        |&nbsp;Bytes
+
+  if item.rawdata
+    h2 Raw data
+    p= JSON.stringify(item.rawdata)
+```
---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var axios = require('axios');
+
+class DSpaceElement {
+    constructor(dspace_base, rest_path, type='root', uuid=null) {
+        this.dspace_base = dspace_base;
+        this.rest_path = rest_path;
+        this.rest_base = dspace_base+rest_path;
+        this.uuid = uuid;
+        this.type = type;
+        this.name = null;
+        this.communities = [];
+        this.collections = [];
+        this.metadata = [];
+        this.bitstreams = [];
+        this.ancestors = [];
+        this.rawdata = null;
+        this.items = [];
+    }
+
+    _readAncestors(data) {
+        this.rawdata = data;
+        var ancestors = [];
+        if(data && data.parentCommunityList)
+            data.parentCommunityList.forEach(({name, uuid}) => { let type='communities'; ancestors.push({name, uuid, type}) });
+        if(data && data.parentCollectionList)
+            data.parentCollectionList.forEach(({name, uuid}) => { let type='collections'; ancestors.push({name, uuid, type}) });
+        this.ancestors = ancestors;
+    }
+}
+
+const items_mapper = ({name, uuid, id}) => {
+    if( !uuid ) uuid=id;
+    return {name, uuid};
+};
+
+class Root extends DSpaceElement {
+    async load() {
+        const url = this.rest_base + "/communities/top-communities";
+        let response = await axios.get(url);
+        this.communities = response.data.map( items_mapper );
+    }
+}
+class Community extends DSpaceElement {
+    async load() {
+        const url = this.rest_base + `/communities/${this.uuid}?expand=all`;
+        let response = await axios.get(url);
+        if(response.data.communities)
+            this.communities = response.data.communities.map( items_mapper );
+        if(response.data.collections)
+            this.collections = response.data.collections.map( items_mapper );
+        this.name = response.data.name;
+        this._readAncestors(response.data);
+    }
+}
+class Collection extends DSpaceElement {
+    async load() {
+        const url = this.rest_base + `/collections/${this.uuid}?expand=all`;
+        let response = await axios.get(url);
+        if(response.data.collections)
+            this.collections = response.data.collections.map( items_mapper );
+        if(response.data.items)
+            this.items = response.data.items.map( items_mapper );
+        this.name = response.data.name;
+        this._readAncestors(response.data);
+    }
+}
+class Item extends DSpaceElement {
+    async load() {
+        let url = this.rest_base + `/items/${this.uuid}?expand=all`;
+        let dspace_base = this.dspace_base;
+        let response = await axios.get(url);
+        if(response.data.metadata)
+            this.metadata = response.data.metadata.map(c => c);
+        if(response.data.bitstreams)
+//            this.bitstreams = response.data.bitstreams.map(c => { c.retrieveUrl = dspace_base+c.retrieveLink; return c});
+            this.bitstreams = response.data.bitstreams.map(c => { c.retrieveUrl = dspace_base+c.link+"/retrieve"; return c; });
+        this.name = response.data.name;
+        this._readAncestors(response.data);
+    }
+}
+
+class DSpace {
+    static connect(dspace_base, rest_path) {
+        return new DSpace(dspace_base, rest_path);
+    }
+    constructor(dspace_base, rest_path) {
+        if( !dspace_base || !rest_path ) throw "DSpace: Base URL and REST API path missing";
+        this.dspace_base = dspace_base;
+        this.rest_path = rest_path;
+        this.rest_base = dspace_base+rest_path;
+    }
+    async getItem(type='root', uuid=null) {
+        let rez;
+        let dspace_base = this.dspace_base;
+        let rest_path = this.rest_path;
+        switch(type) {
+            case 'root':
+                rez = new Root(dspace_base, rest_path, type, uuid);
+                break;
+            case 'communities':
+                rez = new Community(dspace_base, rest_path, type, uuid);
+                break;
+            case 'collections':
+                rez = new Collection(dspace_base, rest_path, type, uuid);
+                break;
+            case 'items':
+                rez = new Item(dspace_base, rest_path, type, uuid);
+                break;
+            default: throw(`DSpace: Wrong Element Type ${type}`);
+        }
+        await rez.load();
+        return rez;
+    }
+}
+
+module.exports = DSpace;
+//module.exports.default = DSpace;
